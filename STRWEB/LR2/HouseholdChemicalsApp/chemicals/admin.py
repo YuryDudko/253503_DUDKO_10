1de38dc54b9acc53407e80883a45f46830bb2d96
Yury <dudcko.yury2016@yandex.ru>
2024-11-01 00:39:47 +0300

    Copy html lab to css initial

---
+++ STRWEB/LR2/HouseholdChemicalsApp/chemicals/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Employee , Product , ProductType , Manufacturer , Order , Customer , Partner , NewsArticle
+from django.contrib.auth.models import User
+
+admin.site.register(Employee)
+admin.site.register(Product)
+admin.site.register(ProductType)
+admin.site.register(Manufacturer)
+admin.site.register(Order)
+admin.site.register(Customer)
+admin.site.register(Partner)
+admin.site.register(NewsArticle)
